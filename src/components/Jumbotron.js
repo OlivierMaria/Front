2a2a5225d85f075ce742f61787fa1a82c27c2306
OlivierMaria <olivier-maria@hotmail.com>
2023-06-12 17:58:49 +0200

    Home

---
+++ src/components/Jumbotron.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import video from "../assets/video/Jumbotron.mp4";
+
+const Jumbotron = () => {
+  return (
+    <div className="relative">
+      <video src={video} autoPlay loop muted className="w-full">
+        Sorry, your browser doesn't support embedded videos.
+      </video>
+      <Link to="/properties">
+        <button className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 px-6 py-3 text-white bg-blue-800 rounded-md hover:bg-blue-600">
+          On y va ⮞
+        </button>
+      </Link>
+    </div>
+  );
+};
+
+export default Jumbotron;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -43,7 +43,7 @@ const NavBar = () => {
             </Link>
           </div>
           <div className="flex items-center">
-            <button className="text-white px-4 py-2 rounded-md bg-blue-600 hover:bg-blue-700 text-sm font-medium">
+            <button className="text-white px-4 py-2 rounded-md bg-blue-800 hover:bg-blue-700 text-sm font-medium">
               Se connecter
             </button>
           </div>
