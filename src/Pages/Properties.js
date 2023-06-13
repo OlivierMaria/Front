f0a67c83ef284d91928ab141a95362d36c3dfa7c
OlivierMaria <olivier-maria@hotmail.com>
2023-06-13 11:41:17 +0200

    SearchBar Mise en place en attende de l'api rails

--- src/Pages/Properties.js
+++ src/Pages/Properties.js
@@ -1,9 +1,10 @@
 import React from "react";
+import SearchBar from "../components/SearchBar.js";
 
 const Properties = () => {
   return (
     <div>
-      <h1>PropertiesPage</h1>
+      <SearchBar />
     </div>
   );
 };
