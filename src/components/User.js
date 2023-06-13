c251cc483fe9d9a34ffae00de6d85f4ad260bb7e
OlivierMaria <olivier-maria@hotmail.com>
2023-06-13 10:23:54 +0200

    Maintient de la session User au raffraichisement de la page

--- src/components/User.js
+++ src/components/User.js
@@ -2,12 +2,28 @@ import Signup from "./Signup";
 import Login from "./Login";
 import Logout from "./Logout";
 import PrivateText from "./PrivateText.js";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
-
-const User = ({ currUser, setCurrUser }) => {
+const User = () => {
+  const [currUser, setCurrUser] = useState(null);
   const [show, setShow] = useState(true);
-  if (currUser)
+
+  useEffect(() => {
+    const storedUser = localStorage.getItem("currentUser");
+    if (storedUser) {
+      setCurrUser(JSON.parse(storedUser));
+    }
+  }, []);
+
+  useEffect(() => {
+    if (currUser) {
+      localStorage.setItem("currentUser", JSON.stringify(currUser));
+    } else {
+      localStorage.removeItem("currentUser");
+    }
+  }, [currUser]);
+
+  if (currUser) {
     return (
       <div>
         Hello {currUser.email}
@@ -15,14 +31,17 @@ const User = ({ currUser, setCurrUser }) => {
         <Logout setCurrUser={setCurrUser} />
       </div>
     );
-  return (
-    <div>
-      {show ? (
-        <Login setCurrUser={setCurrUser} setShow={setShow} />
-      ) : (
-        <Signup setCurrUser={setCurrUser} setShow={setShow} />
-      )}
-    </div>
-  );
+  } else {
+    return (
+      <div>
+        {show ? (
+          <Login setCurrUser={setCurrUser} setShow={setShow} />
+        ) : (
+          <Signup setCurrUser={setCurrUser} setShow={setShow} />
+        )}
+      </div>
+    );
+  }
 };
+
 export default User;
